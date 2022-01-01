3411b05a438ec1ee4d3b6c7b4bb06bb40e8315e5
jmj005 <jmj00005@naver.com>
2022-01-01 21:19:03 +0900

    영상 반전처리 알고리즘

---
+++ computer_vision/complement.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+ FILE *file1, *file2;
+ int image[800][1100];
+ char M;
+ char N;
+ int XX, YY, MAX;
+ int x, y;
+
+ file1=fopen(argv[1], "r");
+ fscanf(file1, "%c%c", &M, &N);
+ fscanf(file1, "%d%d", &XX, &YY);
+ fscanf(file1, "%d", &MAX);
+ for(y=0;y<YY;y++){
+  for(x=0;x<XX;x++)
+   fscanf(file1, "%d", &image[y][x]);
+  }
+
+ for(y=0;y<YY;y++)
+  for(x=0;x<XX;x++)
+   image[y][x] = 255 - image[y][x];
+
+ file2=fopen(argv[2], "w");
+ fprintf(file2, "%c%c\n", M, N);
+ fprintf(file2, "%d %d\n", XX, YY);
+ fprintf(file2, "%d\n", MAX);
+ for(y=0;y<YY;y++){
+  for(x=0;x<XX;x++)
+   fprintf(file2, "%4d", image[y][x]);
+  fprintf(file2, "\n");
+ }
+
+return 0;
+}
